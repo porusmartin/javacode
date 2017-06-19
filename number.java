3a72c46026364eb7b4b707d5e2a5ca66f40ece94
porusmartin <porusmartin.j@3edge.in>
2017-06-19 14:00:55 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Sample {
+
+public static void main(String[] args) {
+	int num=1;
+	if(num==0){
+		System.out.println("Number is zero");
+	}else if(num >0){
+		System.out.println("Number is postive");
+	}else{
+		System.out.println("Number is negative");
+	}
+	}		
+}
